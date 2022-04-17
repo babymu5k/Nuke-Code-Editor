9ca252d20ee23e30692f9421f57b2bacc973963c
superpythonguy <95881676+superpythonguy@users.noreply.github.com>
2022-04-17 19:03:10 +0100

    Add files via upload

---
+++ RUN.py
@@ -0,0 +1,7 @@
+print("""
+      NUKE IDE\n
+      V0.1.0\n
+      developed by SuperPythonGuy
+      """)
+from NUKEIDE import *
+from themes import *
---
+++ themes.py
@@ -0,0 +1,5 @@
+from NUKEIDE import editor
+def devdark():
+    editor.config(fg="white",bg="#494949")
+def monkai():
+    editor.config(fg="#F8F8F2",bg="#272822")
